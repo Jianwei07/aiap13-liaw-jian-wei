7f52c61bddd3dd3a96d11bcdf051224a894a352c
Jianwei07 <113814163+Jianwei07@users.noreply.github.com>
2023-01-12 23:19:12 +0800

    Changes
    Task 1, new visualization
    Task 2, potential MLP.

--- src/MLP_module1.py
+++ src/MLP_module1.py
@@ -1,8 +1,12 @@
 import pandas as pd
+import matplotlib.pyplot as plt
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.svm import SVC
+from sklearn.linear_model import LogisticRegression
 
 # Load the data
 data = pd.read_csv("failures.csv")
@@ -11,11 +15,12 @@ data = pd.read_csv("failures.csv")
 data.dropna(inplace=True)
 data = pd.get_dummies(data)
 
-# Perform feature engineering
-data["new_feature"] = data["feature1"] + data["feature2"]
+# Define the features and target
+X = data.drop("Model", axis=1)
+y = data["Failure"]
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(data.drop("target", axis=1), data["target"], test_size=0.2)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2)
 
 # Scale the data
 scaler = StandardScaler()
@@ -31,4 +36,30 @@ y_pred = clf.predict(X_test)
 
 # Evaluate the model
 acc = accuracy_score(y_test, y_pred)
+precision = precision_score(y_test, y_pred)
+recall = recall_score(y_test, y_pred)
+f1 = f1_score(y_test, y_pred)
 print("Accuracy: ", acc)
+print("Precision: ", precision)
+print("Recall: ", recall)
+print("F1 Score: ", f1)
+
+# Create a dictionary to store the models
+models = {"Random Forest": RandomForestClassifier(),
+          "SVM": SVC(),
+          "Logistic Regression": LogisticRegression()}
+
+# Train and evaluate each model
+for name, model in models.items():
+    # Train the model
+    model.fit(X_train, y_train)
+    
+    # Predict on test set
+    y_pred = model.predict(X_test)
+    
+    # Evaluate the model
+    acc = accuracy_score(y_test, y_pred)
+    precision = precision_score(y_test, y_pred)
+    recall = recall_score(y_test, y_pred)
+    f1 = f1_score(y_test, y_pred)
+    print()
