4a4f8533074d7d2a6bcc89c909f54908f68fcfcd
Jianwei07 <113814163+Jianwei07@users.noreply.github.com>
2023-01-15 00:31:39 +0800

    Update 5
    Added a code to combine the variable into multiple binary

--- src/MLP_module1.py
+++ src/MLP_module1.py
@@ -1,21 +1,20 @@
 import pandas as pd
 from sklearn.preprocessing import StandardScaler
 from sklearn.model_selection import train_test_split
-from sklearn.ensemble import RandomForestClassifier
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.svm import SVC
 from sklearn.linear_model import LogisticRegression
 
 # Load the data
-data = pd.read_csv("data aiap13\failures.csv")
+data = pd.read_csv("C:\\Users\\Jianw\\Documents\\GitHub\\aiap13-liaw-jian-wei-026z\\data aiap13\\failure.csv")
 
 # Perform preprocessing
 data.dropna(inplace=True)
 data = pd.get_dummies(data)
 
 # Define the features and target
-X = data.drop("Model", axis=1)
+X = data.drop("Failure", axis=1)
 y = data["Failure"]
 
 # Split the data into training and test sets
@@ -26,39 +25,21 @@ scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
 X_test = scaler.transform(X_test)
 
-# Train a random forest classifier
-clf = RandomForestClassifier()
-clf.fit(X_train, y_train)
-
-# Predict on test set
-y_pred = clf.predict(X_test)
-
-# Evaluate the model
-acc = accuracy_score(y_test, y_pred)
-precision = precision_score(y_test, y_pred)
-recall = recall_score(y_test, y_pred)
-f1 = f1_score(y_test, y_pred)
-print("Accuracy: ", acc)
-print("Precision: ", precision)
-print("Recall: ", recall)
-print("F1 Score: ", f1)
-
 # Create a dictionary to store the models
 models = {"Random Forest": RandomForestClassifier(),
           "SVM": SVC(),
           "Logistic Regression": LogisticRegression()}
 
-# Train and evaluate each model
 for name, model in models.items():
     # Train the model
     model.fit(X_train, y_train)
-    
+
     # Predict on test set
     y_pred = model.predict(X_test)
-    
+
     # Evaluate the model
     acc = accuracy_score(y_test, y_pred)
     precision = precision_score(y_test, y_pred)
     recall = recall_score(y_test, y_pred)
     f1 = f1_score(y_test, y_pred)
-    print()
+    print(f"{name} - accuracy: {acc}, precision: {precision}, recall: {recall}, f1: {f1}")
