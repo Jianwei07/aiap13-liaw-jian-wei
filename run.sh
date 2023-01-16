1b07172ff5075551b6d98b2b4f24e7c9da88ee36
Jianwei07 <113814163+Jianwei07@users.noreply.github.com>
2023-01-16 08:46:52 +0800

    update on run.sh
    update on run.sh
    
    checks on MLP.py

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
+actions/checkout@v2
 
-C:\\Users\\Jianw\\Documents\\GitHub\\aiap13-liaw-jian-wei-026z\\src\\MLP_module1.py --lr 0.001 --epochs 100 --batch_size 64
+src\MLP_module1.py
+
+chmod +x run.sh
--- src/MLP_module1.py
+++ src/MLP_module1.py
@@ -19,7 +19,7 @@ X = data.drop("Failure", axis=1)
 y = data["Failure"]
 
 # Split the data into training and test sets
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2,random_state=45)
 
 # Train and evaluate a logistic regression model
 log_reg = LogisticRegression()
